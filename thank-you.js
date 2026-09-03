b26522584cf6747b41b4653c3becd6fd27f7ce18
forsrustam-tech <forsrustam@gmail.com>
2026-09-03 15:51:58 +0500

    Remove duplicate Meta Lead logic from thank-you script

--- thank-you.js
+++ thank-you.js
@@ -1,12 +1,6 @@
 (() => {
   const INSTAGRAM_URL = 'https://www.instagram.com/mzosanki/';
   const REDIRECT_SECONDS = 5;
-  const confirmedLead = sessionStorage.getItem('mzoLeadPending') === '1';
-
-  if (confirmedLead && typeof window.fbq === 'function') {
-    window.fbq('track', 'Lead');
-    sessionStorage.removeItem('mzoLeadPending');
-  }
 
   const countdown = document.getElementById('redirect-countdown');
   let seconds = REDIRECT_SECONDS;
